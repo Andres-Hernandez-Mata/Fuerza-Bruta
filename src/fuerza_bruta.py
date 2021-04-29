ff227e679d516132fc1228b74409277fdf2be8cb
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-04-29 14:50:45 -0500

    Alfabeto
    Mayusculas

--- src/fuerza_bruta.py
+++ src/fuerza_bruta.py
@@ -17,7 +17,7 @@ os.system('cls')
 # Recibe una entrada del usuario y almacénala en una variable
 password = getpass.getpass('Ingrese la contraseña: ')
 # Define un arreglo con el alfabeto a utilizar
-alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 # Inicializa una cadena temporal que servirá para armar el texto generado de manera aleatoria
 cadena = ''
 # Registra en una variable la hora antes de comenzar la prueba de fuerza bruta.
@@ -28,7 +28,7 @@ print('\033[0;32m[INFO] Espere un momento... \033[0;0m')
 while cadena != password:
     cadena = ''
     for simbolo in password:
-        indice = random.randint(0,26)        
+        indice = random.randint(0,53)        
         cadena+=alfabeto[indice]
     intentos+=1
 time.sleep(0)
