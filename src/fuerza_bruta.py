b35285d7052af65787d4b6163a27bed274521670
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-05-01 07:59:45 -0500

    Exception
    try

--- src/fuerza_bruta.py
+++ src/fuerza_bruta.py
@@ -15,29 +15,42 @@ import os
 os.system('cls')
 
 # Recibe una entrada del usuario y almacénala en una variable
-password = getpass.getpass('Ingrese la contraseña: ')
-# Define un arreglo con el alfabeto a utilizar
-alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+try:
+    while(True):        
+        password = getpass.getpass('Ingrese la contraseña: ')
+        if password != '':
+            break
+        print(datetime.now(), '\033[0;91m [ERROR] La contraseña es un campo obligatorio \033[0;0m')
+except Exception as error:
+    print(datetime.now(), '\033[0;91m [ERROR] Ha ocurrido un error ')
+    print(error)
+
+# Define un arreglo con el alfabeto a utilizar ,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
 # Inicializa una cadena temporal que servirá para armar el texto generado de manera aleatoria
 cadena = ''
 # Registra en una variable la hora antes de comenzar la prueba de fuerza bruta.
 inicio = datetime.now()
 intentos = 1
 
-print('\033[0;32m[INFO] Espere un momento... \033[0;0m')
-while cadena != password:
-    cadena = ''
-    for simbolo in password:
-        indice = random.randint(0,53)        
-        cadena+=alfabeto[indice]
-    intentos+=1
-time.sleep(0)
-final = datetime.now()
-tiempo = final - inicio
-print('\033[0;32m[INFO] Intentos: \033[0;0m', intentos)
-time.sleep(1)
-print('\033[1;31m[INFO] Contraseña: \033[0;0m', cadena)
-time.sleep(1)
-print('\033[0;32m[INFO] Tiempo: \033[0;0m', tiempo)
+try:
+    while cadena != password:
+        cadena = ''
+        for simbolo in password:
+            indice = random.randint(0,26)        
+            cadena+=alfabeto[indice]
+        intentos+=1
+        os.system("cls")
+        print(datetime.now(), '\033[0;32m[INFO] Espere un momento... \033[0;0m')
+        print(datetime.now(), '\033[0;32m[INFO] Intentos: \033[0;0m', intentos)
+    time.sleep(0)
+    final = datetime.now()
+    tiempo = final - inicio
+    time.sleep(1)
+    print(datetime.now(), '\033[1;31m[INFO] Contraseña: \033[0;0m', cadena)
+    print(datetime.now(), '\033[0;32m[INFO] Tiempo: \033[0;0m', tiempo)
+except Exception as error:
+    print(datetime.now(), '\033[0;91m [ERROR] Ha ocurrido un error ')
+    print(error)
 
 
